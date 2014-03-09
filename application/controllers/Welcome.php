ba3653e7149530859b67f965f6c559e67641a06f
Mohamed Rashid <rashivkp@gmail.com>
2014-03-09 22:56:13 +0530

    adding a simple upload functionality,

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -28,24 +28,24 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see http://codeigniter.com/user_guide/general/urls.html
+    /**
+     * Index Page for this controller.
+     *
+     * Maps to the following URL
+     * 		http://example.com/index.php/welcome
+     *	- or -
+     * 		http://example.com/index.php/welcome/index
+     *	- or -
+     * Since this controller is set as the default controller in
+     * config/routes.php, it's displayed at http://example.com/
+     *
+     * So any other public methods not prefixed with an underscore will
+     * map to /index.php/welcome/<method_name>
+     * @see http://codeigniter.com/user_guide/general/urls.html
      */
 
-	public function index()
-	{
+    public function index()
+    {
         $this->load->view('welcome_message');
     }
 
@@ -66,6 +66,26 @@ class Welcome extends CI_Controller {
         echo "Your Logged IN.";
     }
 
+    public function upload() {
+
+        $config['upload_path'] = './uploads/';
+        $config['allowed_types'] = 'gif|jpg|png';
+        $config['max_size'] = '100';
+        $config['max_width']  = '1024';
+        $config['max_height']  = '768';
+
+        $this->load->library('upload', $config);
+
+        if(  ! $this->upload->do_upload('photo')){
+            $error = array('error' => $this->upload->display_errors());
+            $this->load->view('uploadform', $error);
+        }
+        else{
+            $data = array('upload_data' => $this->upload->data());
+            print 'upload success';
+            print print_r($data);
+        }
+    }
 }
 
 /* End of file welcome.php */
---
+++ application/views/uploadform.php
@@ -0,0 +1,5 @@
+<?=@$error?>
+<form method="post" enctype="multipart/form-data">
+    <input type='file' name='photo' />
+    <input type='submit' value='upload' />
+</form>
